3847724f062c8b870ade1cc1be974e5f16cb6f64
Diego A. Dias <dtdias@icatuseguros.com.br>
2021-10-13 18:23:28 -0300

    Add project files.

---
+++ Calculadora/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Calculadora teste
+
+            /*
+              Explicando c# introdução
+              Calculadora simples adição e subtração
+             */
+
+            //int não tem casa decimal - int32  ou int
+            //breakpoint para debug, parar na linha de usar a tecla f9 para marcar
+            //é possível ver o conteúdo da variável usando o AddWatch
+            
+            int num1; 
+            int num2;
+
+            Console.WriteLine("Calculadora de soma e subtração");
+            Console.WriteLine("*******************************");
+
+            Console.WriteLine("Digite o valor da variável num1");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da variável num2");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            int i = 99; // quero que o i 'vá' para a variável a
+            string a;
+
+            a = i.ToString();
+            a = Convert.ToString(i);
+
+            Console.WriteLine($"Turma {num1}");
+
+            Console.ReadLine(); //serve para travar a tela em alguns visual studio (configuração)
+        }
+    }
+}
